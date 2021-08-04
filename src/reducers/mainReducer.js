cf65c2c4d198cc0ae397ef3efd2741e92a20b6f1
Bolo.js <bolo.js@cch.com>
2021-08-04 10:06:41 +0300

    'start'

---
+++ src/reducers/mainReducer.js
@@ -0,0 +1,14 @@
+import {
+    
+} from '../actions';
+
+const defaultState = {
+
+}
+
+export default (state = defaultState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
